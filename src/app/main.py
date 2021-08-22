80e29f81e98b2142c8931032ab782a4cbc77e95f
Daniel Lima <daniellima.pessoal@gmail.com>
2021-08-22 12:47:40 -0300

    docs: add swagger support

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,18 @@
 from aiohttp import web
+from aiohttp_swagger import setup_swagger
 
 async def hello(request):
+    '''
+    ---
+    description: Retorna um "Hello World" no estilo do JSON
+    tags:
+    - hello word
+    produces:
+    - application/json
+    responses:
+        "200":
+            description: Se considere bem vindo.
+    '''
     return web.json_response({'hello': 'world!'})
 
 app = web.Application()
@@ -8,6 +20,8 @@ app = web.Application()
 app.add_routes([
     web.get('/', hello)
 ])
+
+setup_swagger(app)
 
 if __name__ == '__main__':
     web.run_app(app)
